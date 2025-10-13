eff1e06b3e272173a3f1a7a366d6cc8010d66bd2
Vassiliy Yegorov <va@egorovanet.ru>
2025-10-13 12:30:54 +0700

    Release v1.1.7

--- main.go
+++ main.go
@@ -17,10 +17,10 @@ import (
 )
 
 var (
-	image      string = "factory.talos.dev/metal-installer/6adc7e7fba27948460e2231e5272e88b85159da3f3db980551976bf9898ff64b:v1.11.0"
+	image      string = "factory.talos.dev/metal-installer/6adc7e7fba27948460e2231e5272e88b85159da3f3db980551976bf9898ff64b:v1.11.2"
 	k8sVersion string = "1.34.0"
 	configDir  string = "config"
-	version    = "v1.1.6"
+	version    = "v1.1.7"
 )
 
 const (
